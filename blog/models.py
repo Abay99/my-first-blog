25693607ab16579ce13f6a6cf6dfa73cdd63c736
abai <abai_kalikov@mail.ru>
2018-11-29 19:51:46 +0600

    feat: make uploading photo into post

--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,19 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+def upload_location(post, filename):
+    return "%s/%s" %(post.pk, filename)
+
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
+    image = models.ImageField(upload_to=upload_location,
+                              null=True,
+                              width_field="width_field",
+                              height_field="height_field")
+    width_field = models.IntegerField(default=0)
+    height_field = models.IntegerField(default=0)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 

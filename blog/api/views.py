4ca3b3d59d63004816b06c0864eec455c4df6984
abai <abai_kalikov@mail.ru>
2018-12-15 18:42:11 +0600

    feat: added pagination to api

--- blog/api/views.py
+++ blog/api/views.py
@@ -3,17 +3,20 @@ from blog.models import Post
 from rest_framework import viewsets
 from blog.api.serializers import UserSerializer, PostSerializer
 from rest_framework.generics import UpdateAPIView, DestroyAPIView, ListAPIView
+from .pagination import PostLimitOffsetPagination, PostPageNumberPagination
 
 class UserViewSet(viewsets.ModelViewSet):
 
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+    pagination_class = PostPageNumberPagination
 
 
 class PostViewSet(viewsets.ModelViewSet):
 
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    pagination_class = PostPageNumberPagination
 
 class PostListAPIView(ListAPIView):
     queryset = Post.objects.all()
